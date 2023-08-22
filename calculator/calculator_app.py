17fb59afdfb9d14e3643eba4d40c29f551bf538f
RohitSinha@003 <109851908+leviathan003@users.noreply.github.com>
2023-08-22 06:27:55 +0530

    Add files via upload

---
+++ calculator/calculator_app.py
@@ -0,0 +1,67 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class MainApp(App):
+    def build(self):
+        self.operators=['/','*','+','-']
+        #self.icon("") for top side icon
+        self.last_was_operator=None
+        self.last_button=None
+
+        main_layout=BoxLayout(orientation="vertical")
+        self.screen=TextInput(background_color="black",foreground_color="white",font_size=55,multiline=False,halign="right",readonly=True)
+        main_layout.add_widget(self.screen)
+
+        buttons=[
+            ["7","8","9","/"],
+            ["4","5","6","*"],
+            ["3","2","1","+"],
+            [".","0","C","-"],
+        ]
+        for row in buttons:
+            h_layout=BoxLayout()
+            for label in row:
+                button=Button(
+                    text=label,font_size=30,background_color="gray",pos_hint={"center_x":0.5,"center_y":0.5}
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
+
+        equal_button=Button(
+            text="=",font_size=30,background_color="gray",pos_hint={"center_x":0.5,"center_y":0.5}
+        )
+        equal_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equal_button)
+
+        return main_layout
+    
+    def on_button_press(self,instance):
+        current=self.screen.text
+        button_text=instance.text
+
+        if button_text=="C":
+            self.screen.text=""
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                return 
+            elif current=="" and (button_text in self.operators):
+                return
+            else:
+                new_text=current+button_text
+                self.screen.text=new_text
+
+        self.last_button=button_text
+        self.last_was_operator=self.last_button in self.operators
+    
+    def on_solution(self,instance):
+        text=self.screen.text
+        if text:
+            soln=str(eval(self.screen.text))
+            self.screen.text=soln
+
+if __name__=="__main__":
+    app=MainApp()
+    app.run()
